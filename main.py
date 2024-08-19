29cfe43ee1db0bfd89cc8ff1d1a5b28865034e65
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-08-19 10:52:20 +0530

    created routes

--- main.py
+++ main.py
@@ -1,24 +1,118 @@
-from fastapi import FastAPI, requests
+from fastapi import FastAPI, Depends
 import sqlite3
-from models import Query
+from models import Query, ResponseModel
+from typing import List
 
 app = FastAPI(debug=True)
 q = Query()
 
 
-def by_name(name: str):
-    with sqlite3.connect("pokemon.db") as conn:
-        data = conn.execute(
-            q.by_name,
-            (name,),
+def get_db():
+    db = sqlite3.connect("pokemon.db")
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def format_response(response: List[any]) -> List[ResponseModel]:
+    return [
+        ResponseModel(
+            name=row[0],
+            type=row[1],
+            species=row[2],
+            height=row[3],
+            weight=row[4],
+            abilities=row[5],
+            catch_rate=row[6],
+            base_friendship=row[7],
+            base_exp=row[8],
+            growth_rate=row[9],
+            gender=row[10],
+            hp=row[11],
+            attack=row[12],
+            defense=row[13],
+            sp_attack=row[14],
+            sp_defense=row[15],
+            speed=row[16],
         )
-        return data.fetchall()
+        for row in response
+    ]
 
 
-@app.get("/pokemons/name/{pokemon_name}")
-def get_pokemons_by_name(pokemon_name: str):
-    return by_name(name=pokemon_name)
+@app.get("/pokemons/name/{name}")
+def get_pokemons_by_name(
+    name: str, db: sqlite3.Connection = Depends(get_db)
+) -> List[ResponseModel]:
+    cursor = db.cursor()
+    cursor.execute(q.name_query, (name,))
+    response = cursor.fetchall()
+    response_clean = format_response(response=response)
+
+    return response_clean
+
 
 @app.post("/pokemons/abilities/{abilities}")
-def get_pokemons_by_abilities(abilities: str):
-    pass
+def get_pokemons_by_abilities(
+    abilities: str, db: sqlite3.Connection = Depends(get_db)
+) -> List[ResponseModel]:
+    cursor = db.cursor()
+    abilities_binding = f"%{abilities}%"
+    cursor.execute(q.ability_query, [abilities_binding])
+    response = cursor.fetchall()
+    response_clean = format_response(response=response)
+
+    return response_clean
+
+
+@app.post("/pokemons/type/{type}")
+def get_pokemons_by_type(
+    type: str, db: sqlite3.Connection = Depends(get_db)
+) -> List[ResponseModel]:
+    cursor = db.cursor()
+    type_binding = f"%{type}%"
+    cursor.execute(q.type_query, [type_binding])
+    response = cursor.fetchall()
+    response_clean = format_response(response=response)
+
+    return response_clean
+
+
+@app.post("/pokemons/size")
+def get_pokemons_by_size(
+    parameter: str,
+    height: str | None = None,
+    weight: str | None = None,
+    db: sqlite3.Connection = Depends(get_db),
+) -> List[ResponseModel]:
+    cursor = db.cursor()
+
+    print(height[0], height[1:])
+    if parameter == "h":
+        column = "height"
+        operator = height[0]
+        cursor.execute(
+            q.size_query.format(column=column, operator=operator),
+            [float(height[1:])],
+        )
+        response = cursor.fetchall()
+        response_clean = format_response(response=response[1:])
+    elif parameter == "w":
+        column = "weight"
+        operator = weight[0]
+        cursor.execute(
+            q.size_query.format(column=column, operator=operator),
+            [float(weight[1:])],
+        )
+        response = cursor.fetchall()
+        response_clean = format_response(response=response[1:])
+    elif parameter == "wh":
+        operator_h = height[0]
+        operator_w = weight[0]
+        cursor.execute(
+            q.size_query_both.format(operator_h=operator_h, operator_w=operator_w),
+            [float(height[1:]), float(weight[1:])],
+        )
+        response = cursor.fetchall()
+        response_clean = format_response(response=response[1:])
+    return response_clean
--- models.py
+++ models.py
@@ -2,7 +2,28 @@ from pydantic import BaseModel
 
 
 class Query(BaseModel):
-    by_name: str = "SELECT * FROM pokemons WHERE name=?"
-    by_type: str = "SELECT * FROM pokemons WHERE type like '%?%'"
-    
+    name_query: str = "SELECT * FROM pokemons WHERE name=?"
+    type_query: str = "SELECT * FROM pokemons WHERE type like ?"
+    ability_query: str = "SELECT * FROM POKEMONS WHERE abilities like ?"
+    size_query: str = "SELECT * FROM POKEMONS WHERE {column} {operator} ?"
+    size_query_both: str = "SELECT * FROM POKEMONS WHERE height {operator_h} ? AND weight {operator_w} ?"
 
+
+class ResponseModel(BaseModel):
+    name: str
+    type: str
+    species: str
+    height: float | str
+    weight: float | str
+    abilities: str
+    catch_rate: int | str
+    base_friendship: int | str
+    base_exp: int | str
+    growth_rate: str | str
+    gender: str
+    hp: int | str
+    attack: int | str
+    defense: int | str
+    sp_attack: int | str
+    sp_defense: int | str
+    speed: int | str
