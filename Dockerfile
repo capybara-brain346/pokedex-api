1748e13d1cf2b4a023a899bcf90b0bd2833f6357
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-11-08 07:59:44 +0530

    updated requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,7 @@ COPY ./requirements.txt /app/requirements.txt
 
 RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 
-RUN pip install "fastapi[standard]"
-
-RUN pip install psycopg2-binary
-
 COPY ./api /app/api
-
-COPY pokemon.db /app/
 
 EXPOSE 8080
 
--- combine_csv.py
+++ combine_csv.py
@@ -1,29 +1,8 @@
-"""
-Modules to combine multiple csv into one.
-"""
-
 import glob
 import pandas as pd
 
 
 def combine_csv_files(file_pattern: str, output_file: str) -> None:
-    """
-    Combines multiple CSV files into a single CSV file.
-
-    Args:
-        file_pattern (str): The file pattern to match CSV files (e.g., '*.csv').
-        output_file (str): The name of the output file where the combined data will be saved.
-
-    Returns:
-        None: This function does not return any value.
-
-    Notes:
-        - Uses `glob` to find all files matching the given pattern.
-        - Reads each CSV file into a DataFrame and concatenates them.
-        - Saves the combined DataFrame to the specified output file.
-        - Prints a message indicating the number of files combined and the output file name.
-    """
-
     csv_files = glob.glob(file_pattern)
     df_list = [pd.read_csv(file) for file in csv_files]
     combined_df = pd.concat(df_list, ignore_index=True)
